e7df1e0e54c7dbae0d5043e60d2246a92ef7d231
Ricardo Colmenarez <rcolmenarez0110@gmail.com>
2017-03-07 16:34:27 -0400

    Task 3 Terminada

--- 03-nodejs/index.js
+++ 03-nodejs/index.js
@@ -24,6 +24,9 @@ I found it in stack overflow
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const json2csv = require('json2csv');
+const fs = require('fs');
+const path= require('path');
 
 // Setup database
 mongoose.Promise = Promise;
@@ -36,3 +39,27 @@ const app = express();
 // TODO
 
 app.listen(3000);
+app.get('/users', function (req, res){
+    User.find({}, function(err, users){
+        if(err){
+            console.log(err);
+        } else{
+            try {
+                var result = json2csv({data: users, fields: ['name', 'email']});
+                fs.writeFile(__dirname+'/file.csv', result, function(err) {
+                    if (err) throw err;
+                    console.log('file saved');
+                    res.download(__dirname + '/file.csv', 'file.csv');
+                });
+            } catch (err) {
+                // Errors are thrown for bad options, or if the data is empty and no fields are provided.
+                // Be sure to provide fields if it is possible that your data array will be empty.
+                console.error(err);
+            }
+        }
+    })
+});
+
+console.log(`Primero se consulta la BD y se extraen todos los resultados, luego a través de una librería llamada `+
+            `json2csv se convierten los resultados de Formato JSON a csv, por último se escriben en un archivo llamado`+
+            `file.csv y se coloca en la respuesta para la descarga.`);
