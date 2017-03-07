d76a0c13f9e350eaad04be5c1aab498163e64ddc
Ricardo Colmenarez <rcolmenarez0110@gmail.com>
2017-03-07 15:38:37 -0400

    Task 5 Terminada

--- 05-What do you think/WhatDoYouThink.js
+++ 05-What do you think/WhatDoYouThink.js
@@ -1,9 +1,13 @@
 Question:
 
 ¿Horizontal and vertical server scaling, what is better?
-//Please your answer here
-//
+Horizontal Scalling, tener un ambiente distribuido con un balanceador de cargas entre los servidores, ayuda a manejar
+las peticiones de manera mas efectiva.
+
 
 
 You have a database that is responding slow. What would you do?"
-//Please your answer here
+Hay varias posibles soluciones para esto. Se podrían realizar trabajos de Clustering en las tablas (hablando de BD relacionales)
+o quizás realizar Tunning de los Procedures que corren dentro de la BD.
+
+Para las BD no relacionales, realizar un Horizontal Scalling ayuda a aligerar la carga de los servidores
