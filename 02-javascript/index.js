87f76d59a1a2aa1858712f7d2bfb56ffa6e083a4
Ricardo Colmenarez <rcolmenarez0110@gmail.com>
2017-03-07 15:08:53 -0400

    Task 2 Terminada

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -22,7 +22,13 @@ I found it in stack overflow
 // Add fetch polyfill for Node.js
 require('isomorphic-fetch'); // See: https://github.com/matthew-andrews/isomorphic-fetch
 
-function requester(method, base, headers = { Accept: '*/*' }) {
-  return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
-    .then(r => r.json());
-}
+//function requester(method, base, headers = { Accept: '*/*' }) {
+//  return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
+//    .then(r => r.json());
+//}
+
+console.log(`El código esta hecho en ES6. Recibe como parametros el método a utilizar para la petición WEB ` +
+            `ademas de una URL Base a la cuál se le realiza la petición. Utiliza condicionales ternarias para la ` +
+            `validación de campos vacíos ademas de funciones JavaScript para concatenar strings. Además de esto utiliza` +
+            `la función Arrow de ES6 para la declaración de funciones y trabaja con el sistema de promesas de ES6` +
+            `por último devuelve a traves de un archivo JSON la información solicitada en el request.`);
