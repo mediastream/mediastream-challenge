850b3cb98b3bc0e51291e764109095f5f4490ae1
Carlos <crtx000@gmail.com>
2017-03-30 00:45:09 -0400

    Exercises 1 solved. Exercise 2 answered below the questions. Exercise 3 solved. Exercise 4 unsolved (I have no practical knowledge about react.js)

--- 01-basic-cs/index.js
+++ 01-basic-cs/index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 console.log(`
 1.
 ---
@@ -25,13 +24,22 @@ Hat(32266d28-5092-4a69-afb3-90fafd46e04a) sold 9.
 -> Expected result: 7 + 7 + 9 => 23
 `);
 
-const _ = require('lodash'); // https://lodash.com/docs/4.17.4
+const _ = require('underscore');
 const assert = require('assert');
 
 const database = require('./database.json');
+var a = performance.now();
+var array=[];
+_.each(_.pluck(_.filter(database, function(user){ return user.hats.length!=0; }),"hats"),function(hats){
+	_.each(hats,function(hat){
+		array.push(hat);
+	});
+});//This gets me all sold hats in one array
 
-
-const total = 0 // TODO
+const total =_.reduce(_.sortBy(_.countBy(array,"id"),function(num){ return -num;}).slice(0,3), function(memo, num){ return memo + num; }, 0);
+//This allows me to count the sells of every different hat, rank them from top sold to less sold and sum the three top sold.
+var b =performance.now();
+console.log(a+" "+b);
 
 // Throws error on failure
 assert.equal(total, 23, `Invalid result: ${total} != 23`);
