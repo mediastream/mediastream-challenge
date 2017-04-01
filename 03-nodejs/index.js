e7570134c6bac41f8cdcc907bdb94fe9a6f95040
fellipelagos <felipelagos@MacBook-Pro-de-Felipe.local>
2017-03-31 22:55:09 -0300

    Solution to problem 3

--- 03-nodejs/index.js
+++ 03-nodejs/index.js
@@ -18,6 +18,7 @@ $ node utils/seed.js
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const json2csv = require('express-json2csv');
 
 // Setup database
 mongoose.Promise = Promise;
@@ -27,6 +28,23 @@ const User = require('./models/User');
 // Setup Express.js app
 const app = express();
 
-// TODO
+app.use(json2csv);
+app.get('/users', (req, res) => {
+  let user = mongoose.model('User');
+  let query = user.find({}).select('name email -_id');
+  query.exec({}, (err, users) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    let columns = [{
+      prop: 'name',
+      label: 'name'
+    }, {
+      prop: 'email',
+      label: 'email'
+    }];
+    res.csv('users', users, columns, {includeHeader: true});
+  });
+});
 
 app.listen(3000);
