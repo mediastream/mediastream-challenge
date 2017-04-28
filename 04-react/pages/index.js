a6753931af0b59fa3cdc7fbd6244404499896638
Axel García <axel.garciat.ventura@gmail.com>
2017-04-28 00:28:40 -0500

    React optimized list components
    Note: Redux would help us make these components better =)

--- 04-react/pages/index.js
+++ 04-react/pages/index.js
@@ -25,6 +25,8 @@ Example:
 `);
 
 import React from 'react';
+import moment from 'moment';
+import _ from 'lodash';
 
 export default class MyApp extends React.Component {
   render() {
@@ -45,8 +47,86 @@ export default class MyApp extends React.Component {
 
 
 class List extends React.Component {
-  // TODO
+  /**
+   * Optional header
+   * @type {DOMElement|null}
+   */
+  header = null;
+
+  /**
+   * Date display format
+   * @type {String}
+   */
+  format = 'DD/MMM/YYYY';
+
+  /**
+   * Holds each row of the list
+   * @type {Array}
+   */
+  rows = [];
+
   render() {
-    return null;
+    // Get the optional header
+    if (this.props.children) {
+      this.header = <header>{this.props.children}</header>;
+    }
+
+    // Validate and parse the dates
+    _.each(this.props.dates, (isoDate, index) => {
+      const date = moment(isoDate);
+      if (!date.isValid()) {
+        return true;
+      }
+
+      const formattedDate = date.format(this.format).toLowerCase();
+
+      this.rows.push(<Row key={index} index={index} content={formattedDate} />);
+    });
+
+    return (
+      <div className="list">
+        {this.header}
+        <ul>{this.rows}</ul>
+      </div>
+    );
+  }
+}
+
+class Row extends React.Component {
+  /**
+   * This row index
+   * @type {Number}
+   */
+  index = this.props.index;
+
+  /**
+   * This row content
+   * @type {[type]}
+   */
+  content = this.props.content;
+
+  /**
+   * Limit this component update
+   */
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.index !== nextProps.index) {
+      return true;
+    }
+
+    if (this.props.content !== nextProps.content) {
+      return true;
+    }
+
+    return false;
+  }
+
+  render() {
+    const showIndex = (e) => {
+      alert(this.index);
+    };
+
+    return (
+      <li key={this.index} onClick={showIndex}>{this.content}</li>
+    );
   }
 }
