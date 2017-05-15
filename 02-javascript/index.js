0735df83e5c898205bc6595c08201a36a68b3b72
Ignacio Palma <naxopalma@gmail.com>
2017-05-15 13:07:14 -0400

    solutions ignacio palma

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -1,19 +1,32 @@
 'use strict';
 
-console.log(`
-2.
----
+// console.log(`
+// 2.
+// ---
 
-Take a look at the 'requester' function.
+// Take a look at the 'requester' function.
 
-- What it does?
-- How it's used? Add different use-case examples that covers every functionality.
-- How it is called this design pattern or technique?
+// - What it does?
+// -  It basically sets a type of request that can be used with different paths.It returns a function that accepts a path argument that otherwise will be set to an empty array. this.function will perform a network request to the given arguments. base for the url, method for configuring the verb(GET, POST) and headers that are by default are set to allow all. It will eventually parse the response as json.
 
-HINT: Use https://api.github.com/users/mediastream
-`);
+// - How it's used? Add different use-case examples that covers every functionality.
+var makeRequest = requester(GET, http://mysite.com/);
 
-// Add fetch polyfill for Node.js
+makeRequest(['employees']);
+
+var makeRequest = requester(GET, http://mysite.com/, {Access-Control-Allow-Origin: http://www.example.com});
+
+makeRequest('documents', 'posts');
+
+
+// - How it is called this design pattern or technique?
+
+// HINT: Use https://api.github.com/users/mediastream
+// `);
+
+// // Add fetch polyfill for Node.js
+
+//require('es6-promise').polyfill();
 require('isomorphic-fetch'); // See: https://github.com/matthew-andrews/isomorphic-fetch
 
 function requester(method, base, headers = { Accept: '*/*' }) {
--- 03-nodejs/index.js
+++ 03-nodejs/index.js
@@ -1,23 +1,27 @@
 'use strict';
 
-console.log(`
-3.
----
+// console.log(`
+// 3.
+// ---
 
-We need to create a route that downloads the entire database to a .csv file.
-The endpoint must be set to: GET /users
+// We need to create a route that downloads the entire database to a .csv file.
+// The endpoint must be set to: GET /users
 
-Make sure to have an instance of MongoDB running at: mongodb://localhost
+// Make sure to have an instance of MongoDB running at: mongodb://localhost
 
-Run the database seed with:
-$ node utils/seed.js
+// Run the database seed with:
+// $ node utils/seed.js
 
--> Warning: It contains hundreds of entities and our production server is quite small
-`);
+// -> Warning: It contains hundreds of entities and our production server is quite small
+// `);
 
 const express = require('express');
+// const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const csvWriter = require('csv-write-stream')
+const writer = csvWriter()
+const fs = require('fs');
 
 // Setup database
 mongoose.Promise = Promise;
@@ -27,6 +31,16 @@ const User = require('./models/User');
 // Setup Express.js app
 const app = express();
 
-// TODO
+app.get('/users', function(req, res) {
+	writer.pipe(fs.createWriteStream('out.csv'))
+	User.find({}, function (err, users) {
+		users.forEach(function(user) {
+			writer.write({name: user.name, email: user.email})
+		})
+		writer.end()
+	})
+})
 
-app.listen(3000);
+app.listen(3000, function() {
+  console.log('Node app is running on port.. 3000');
+});
