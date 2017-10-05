795d0b148843a6b445a1f4870c296a6cdf13244f
santiago rodriguez <santiago.r@globant.com>
2017-10-05 17:21:36 -0500

    03-nodejs complete;

--- 03-nodejs/src/routes/users.js
+++ 03-nodejs/src/routes/users.js
@@ -1,17 +1,26 @@
 import express from "express";
 import User from "../models/User";
+import json2csv from "json2csv";
 
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
+    var fields = ['name','email'] ;
     User
     .find({})
     .then(userRecords => {
-        res.json({ user: userRecords });
+         var result = json2csv({ data: userRecords, fields: fields });
+         res.attachment('users.csv')
+         res.setHeader('Content-Type', 'application/text-csv')
+         res.end(result)
     })
-    .catch(err => res.status(400).json({ errors: err.errors }));
+     .catch(err => res.status(400).json({ errors: err.errors }));
 });
 
 export default router;
 
+// //another solution
+// const userss =  User.findAndStreamCsv({})
+// fs.createWriteStream('users.csv'));
+// https://www.npmjs.com/package/mongoose-to-csv
