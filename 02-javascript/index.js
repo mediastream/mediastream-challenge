1a3eec680de018f3602b9a52fbabd48e5696422c
santiago rodriguez <santiago.r@globant.com>
2017-10-09 17:23:53 -0500

    test 02 js

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -17,6 +17,20 @@ HINT: Use https://api.github.com/users/mediastream
 require('isomorphic-fetch'); // See: https://github.com/matthew-andrews/isomorphic-fetch
 
 function requester(method, base, headers = { Accept: '*/*' }) {
-  return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
+  return (path = [] , body= {}) => fetch((base ? [base, ...path] : path).join('/'), { method, headers ,body })
     .then(r => r.json());
 }
+
+//1 rutas absolutas, rutas y relativas solo una configuracion
+var get = requester("get",'https://api.github.com' , "application/json");
+
+ get(["users","mediastream"]).then(data => console.log('data',data)).catch(err=>console.log("err",err))
+
+//2 get(["users","mediastream"]).then(data => console.log('data',data)).catch(err=>console.log("err",err))
+//  get(["users","mediastream" ,"repos"]).then(data => console.log('repos___',data)).catch(err=>console.log("err2",err))
+//  agrege body para poder usar post
+//  post(["users","mediastream","gists"] , {gist_id : 1}).then(data => console.log('data',data)).catch(err=>console.log("err",err)) //
+
+
+
+//3 el patron usado es Curryng : https://blog.benestudio.co/currying-in-javascript-es6-540d2ad09400
