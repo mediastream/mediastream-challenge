f56c588428a25ad52d9977594d8849143c2f3b52
Manuel Maldonado <mmaldonadortiz92@gmail.com>
2017-10-26 20:42:35 -0300

    tests 1 and 2 completed

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -20,3 +20,42 @@ function requester(method, base, headers = { Accept: '*/*' }) {
   return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
     .then(r => r.json());
 }
+
+console.log(`
+- What it does?
+
+  it's a wrapper for the fetch api, useful to pre-configure HTTP requests.
+
+- How it's used? Add different use-case examples that covers every functionality.
+
+  the function recieves three parameters
+  - method (GET, POST, PATCH, PUT, DELETE)
+  - base (the url origin)
+  - headers (object to set the headers of the following requests)
+
+  to make a request with this functions it is needed to call it, in order to
+  configure the request, and then call the returned function with an array
+  containing the parts of the url pathname.
+
+  use cases:
+  - 1: direct call
+
+    requester('GET', 'https://api.github.com')(['users', 'mediastream'])
+      .then(response => console.log(response))
+
+  - 2: previously configurated object
+
+    apiReq = requester('GET', 'https://api.github.com')
+
+    apiReq(['users', 'mediastream'])
+      .then(response => console.log(response))
+
+    apiReq(['users', 'hombrew'])
+      .then(response => console.log(response))  
+
+- How it is called this design pattern or technique?
+
+  function closures
+
+  reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+`)
