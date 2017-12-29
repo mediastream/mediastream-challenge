c5350647c0ae21a34e4e5bcd2798f5b186ddfec0
David De Freitas <davidjdr@gmail.com>
2017-12-29 16:30:12 -0300

    finish second challenge

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -20,3 +20,37 @@ function requester(method, base, headers = { Accept: '*/*' }) {
   return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
     .then(r => r.json());
 }
+
+console.log(`
+	Response:
+	The function require 3 parameters, the first is mandatory, 
+    if the second parameter is not provide, use the path
+    the headers params have a default value
+    use spread operator: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Spread_operator
+	use the fetch method to retrieve data from a URL and corvert the result in JSON format
+`);
+
+requester('GET', 'https://api.github.com/users/mediastream')()
+.then((res)=>{
+	console.log('\n\n ***Case 1:\n ',res, '\n\n');
+})
+
+requester('GET')(['https://api.github.com/users/mediastream'])
+.then((res)=>{
+	console.log('\n\n ***Case 2:\n ',res, '\n\n');
+})
+
+requester('GET', 'https://api.github.com')(['users', 'mediastream'])
+.then((res)=>{
+	console.log('\n\n ***Case 3:\n ',res, '\n\n');
+})
+
+requester('GET', 'https://api.github.com/users')(['mediastream'])
+.then((res)=>{
+	console.log('\n\n ***Case 4:\n ',res, '\n\n');
+})
+
+requester('GET')(['https://api.github.com','users','mediastream'])
+.then((res)=>{
+	console.log('\n\n ***Case 5:\n ',res, '\n\n');
+})
