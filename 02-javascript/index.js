2fc1e59b1f6052273efb55100c0bf8bc5f60ae7f
Martin Ocando <martinocandop@gmail.com>
2018-11-21 20:27:51 -0400

    2) Get gits and remove org

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -6,9 +6,9 @@ console.log(`
 
 Take a look at the 'requester' function.
 
-- What it does?
-- How it's used? Add different use-case examples that covers every functionality.
-- How it is called this design pattern or technique?
+1) What it does?
+2) How it's used? Add different use-case examples that covers every functionality.
+3) How it is called this design pattern or technique?
 
 HINT: Use https://api.github.com/users/mediastream
 `);
@@ -16,7 +16,28 @@ HINT: Use https://api.github.com/users/mediastream
 // Add fetch polyfill for Node.js
 require('isomorphic-fetch'); // See: https://github.com/matthew-andrews/isomorphic-fetch
 
-function requester(method, base, headers = { Accept: '*/*' }) {
-  return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
-    .then(r => r.json());
+const requester = (method, base, headers = { Accept: '*/*' } ) => 
+  (path = []) =>
+    fetch((base ? [base, ...path] : path).join('/'), { method, headers }).then(r => r.json());
+
+const base = 'https://api.github.com/users/mediastream'
+const methods = { GET: 'GET', DELETE: 'DELETE' }
+const token = "eyxXSADJSAIDJSADIOJASOIXJI"
+const deletableOrgId = 1
+
+console.log(`
+  1) Function that return another function whose value is the fetch promise response (resolved or rejected) and we need to pass params for usage
+  3) Technique: High Order Function
+`)
+async function main() {
+  const getGistsResponse = await requester(methods.GET, base)(['gists'])
+  console.log(`Count of gists: ${getGistsResponse.length}`)
+
+  const deleteOrgResponse = await requester(methods.DELETE, base, {
+    Authorization: token
+  })([`orgs/${deletableOrgId}`])
+  console.log('Delete org response')
+  console.log(deleteOrgResponse)
 }
+
+main()
