64251e7b514ccf1bde1e22e4484c82b6f9f114b0
Luis Porras <lporras16@gmail.com>
2018-11-29 11:46:23 -0300

    solving 01-basic-cs

--- 01-basic-cs/index.js
+++ 01-basic-cs/index.js
@@ -9,7 +9,7 @@ Find the total sum of the top-3 most selling hats.
 We don't care which hats are.
 You can use lodash/underscore (recommended)
 
-What is the complexity in O() notation of time and space?
+What is the complexity in O() notation of time and space? n elevated to 2
 
 IMPORTANT: Find a balance between performance and legibility (more important).
 
@@ -30,8 +30,28 @@ const assert = require('assert');
 
 const database = require('./database.json');
 
+let total = 0 // TODO
+let hatsSoldInfo = {};
 
-const total = 0 // TODO
+// Counting solds from hats
+database.forEach((user) => {
+    user.hats.forEach((hat) => {
+        if (hatsSoldInfo[hat.id] != undefined) {
+            hatsSoldInfo[hat.id] = hatsSoldInfo[hat.id] + 1;
+        } else {
+            hatsSoldInfo[hat.id] = 1;
+        }
+    });
+});
+
+// Convert hatsSoldInfo from json to array of pairs
+let hatsPairs = _.toPairs(hatsSoldInfo);
+//Sorting hatsPairs in desc order by number of solds
+hatsPairs.sort((a, b) => {
+    return b[1] - a[1];
+});
+
+total = hatsPairs[0][1] + hatsPairs[1][1] + hatsPairs[2][1];
 
 // Throws error on failure
 assert.equal(total, 23, `Invalid result: ${total} != 23`);
