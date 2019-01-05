206d24cbd5a4b7031e07ea57234e300c0300645a
Nelson Torres <nelsondaniel88@gmail.com>
2019-01-05 15:14:41 -0400

    This is my approach to the third test

--- 03-nodejs/index.js
+++ 03-nodejs/index.js
@@ -18,10 +18,20 @@ $ node utils/seed.js
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const json2csv = require('json2csv');
+const fs = require('fs');
+
 
 // Setup database
+
+//This method is not deprecated
+mongoose.createConnection('mongodb://127.0.0.1:27017/mediastream-challenge', {
+  useMongoClient: true
+  
+})
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/mediastream-challenge');
+//this method is deprecated
+// mongoose.connect('mongodb://localhost/mediastream-challenge');
 const User = require('./models/User');
 
 // Setup Express.js app
@@ -29,4 +39,19 @@ const app = express();
 
 // TODO
 
+
+app.get('/', function (req, res) {
+  User.find({},(error, users) => {
+    console.error(error)
+    let fields = ['id', 'name', 'email'];
+    res.set("Content-Disposition", "attachment;filename=user.csv");
+    res.set("Content-Type", "application/octet-stream");
+    res.send(json2csv({ data: users, fields: fields }))
+  })
+});
+
+
+
 app.listen(3000);
+
+
