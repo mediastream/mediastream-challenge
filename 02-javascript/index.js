f8aeece7a24ae5eb4c3e410e98d30b6a341f196b
Nelson Torres <nelsondaniel88@gmail.com>
2019-01-05 10:48:54 -0400

    second finish Nelson

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -7,9 +7,13 @@ console.log(`
 Take a look at the 'requester' function.
 
 - What it does?
-- How it's used? Add different use-case examples that covers every functionality.
-- How it is called this design pattern or technique?
+    It does an HTTP request to an url that you provide and you will get json with the proper data
+    that you requested 
 
+- How it's used? Add different use-case examples that covers every functionality.
+  check in the code below 
+- How it is called this design pattern or technique?
+    this technique is called currying
 HINT: Use https://api.github.com/users/mediastream
 `);
 
@@ -20,3 +24,8 @@ function requester(method, base, headers = { Accept: '*/*' }) {
   return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
     .then(r => r.json());
 }
+
+//this is the common way of use it, you use the method you want in this case
+// I use GET and then choose the url that you want to use and finish
+requester('get', 'https://api.github.com/users/mediastream')().then(console.log)
+
