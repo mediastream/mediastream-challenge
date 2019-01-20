4cc3bbe3efd8acdcff0321872f598e0ed99fa17f
Manuel Villafañe <manuel.villafane@engiefactory.com>
2019-01-19 23:21:03 -0300

    Save 02 changes - What it does?   get data from a path passed as an argument - How it is called this design pattern or technique?   IIFE (Immediately-Invoked-Function-Expressions)

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -6,7 +6,7 @@ console.log(`
 
 Take a look at the 'requester' function.
 
-- What it does?
+- What it does? 
 - How it's used? Add different use-case examples that covers every functionality.
 - How it is called this design pattern or technique?
 
@@ -20,3 +20,11 @@ function requester(method, base, headers = { Accept: '*/*' }) {
   return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
     .then(r => r.json());
 }
+
+(async () => {
+  const example1 = await requester('get', 'https://api.github.com')(['users', 'mediastream']);
+  console.log(example1);
+  console.log('______________________________________________________');
+  const example2 = await requester('get', 'https://api.github.com', { 'Content-Type': 'application/json' })(['users', 'mediastream']);
+  console.log(example2);
+})()
