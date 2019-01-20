b426b116d80afa706e72b19731874e6e5b6bc5b8
Alvaro Neira <alvaroneirareyes@gmail.com>
2019-01-20 19:58:07 -0300

    WIP

--- 03-nodejs/index.js
+++ 03-nodejs/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const BUF_SIZE = 10;
+const SEPARATOR = ',';
+const FILENAME = "users_file.csv";
+
 console.log(`
 3.
 ---
@@ -18,7 +22,8 @@ $ node utils/seed.js
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const fs = require('fs');
+const _ = require('lodash');
 // Setup database
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/mediastream-challenge');
@@ -26,7 +31,43 @@ const User = require('./models/User');
 
 // Setup Express.js app
 const app = express();
+app.get('/users', downloadUsers);
 
-// TODO
+function downloadUsers(req,res){
+    let reader = User.find().stream();
+
+    reader.on('error', function (err) {
+        console.error('error = ' + err);
+        res.send('error');
+    });
+
+    reader.on('data', function (doc) {
+        writeChunk(writer, doc);
+    });
+
+    reader.on('end', function () {
+        console.log('end event');
+    });
+    reader.on('close', function () {
+        console.log('close event');
+        res.send('ready');
+    });
+    reader.on('exit', function (code, signal) {
+        console.log('exit event, code: ' + code + ', signal: ' + signal);
+        res.send('exit');
+    });
+};
 
 app.listen(3000);
+let writer = fs.createWriteStream(FILENAME, {flags: 'w'});
+writer.write('name' + SEPARATOR + 'email\n');
+
+function writeChunk(outStream, _users) {
+    let users = _users;
+    if (!_.isArray(users)) {
+        users = [_users];
+    }
+    _.forEach(users, function (user) {
+        outStream.write(user.name + SEPARATOR + user.email + '\n');
+    });
+}
