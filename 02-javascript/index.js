cbb76c12fc572b22e2f22210d335a21062046ed5
Guillermo Ahumada <gahumada@cocha.com>
2019-01-26 17:20:12 -0300

    solution test 2

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -7,8 +7,11 @@ console.log(`
 Take a look at the 'requester' function.
 
 - What it does?
+  execute an HTTP request to a url that is delivered to you by parameter and you will get the right data
 - How it's used? Add different use-case examples that covers every functionality.
+
 - How it is called this design pattern or technique?
+  the technique is called currying, normally used in functional programming
 
 HINT: Use https://api.github.com/users/mediastream
 `);
@@ -20,3 +23,24 @@ function requester(method, base, headers = { Accept: '*/*' }) {
   return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
     .then(r => r.json());
 }
+
+requester('get', 'https://api.github.com/users/mediastream')()
+  .then(
+    data => console.log('example 1: ', data)
+  )
+
+let request = new requester('get', 'https://api.github.com/users');
+request(['mediastream']).then(
+  data => console.log('example 2: ', data)
+  )
+  
+request = new requester('get', 'https://api.github.com');
+request(['users', 'mediastream'])
+  .then(
+    data => console.log('example 3: ', data)
+  )
+  
+request(['users', '00guille'])
+  .then(
+    data => console.log('example 4 reusing requester instance: ', data)
+  )
