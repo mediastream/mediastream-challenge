4959c898505b8423bee3fdfa5b5c8a2433f85d38
Sebastián Silva <sebasilvac88@gmail.com>
2019-10-04 12:54:04 -0300

    Ejercicio número 03
    se instala librería json2csv

--- 03-nodejs/index.js
+++ 03-nodejs/index.js
@@ -18,6 +18,7 @@ $ node utils/seed.js
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const { Parser } = require('json2csv');
 
 // Setup database
 mongoose.Promise = Promise;
@@ -26,7 +27,36 @@ const User = require('./models/User');
 
 // Setup Express.js app
 const app = express();
+app.use(morgan('tiny'))
 
-// TODO
+app.get("/users", async function (req, res) {
+    
+    const listUsers = await getUsers();
+    const fields = ['name', 'email'];
+    const json2csvParser = new Parser({ fields });
+    const csv = json2csvParser.parse(listUsers);
+
+    res.setHeader('Content-disposition', 'attachment; filename=listUsers.csv');
+    res.set('Content-Type', 'text/csv');
+    res.status(200).send(csv);
+});
 
 app.listen(3000);
+
+/**
+ * retorna la lista de usuarios
+ */
+async function getUsers() {
+
+    const listUsers = await User.find({}, function(err, users){
+
+        if( err ) {
+            console.log(err);
+            return;
+        }
+    
+        return users;
+    });
+
+    return listUsers;
+}
