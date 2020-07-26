8eef569a9839d40e71856bc7e822f6aa5ef43286
AryRosvall <ary.rosvall@gmail.com>
2020-07-25 20:53:29 -0500

    Challenge 02 completed

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -7,8 +7,16 @@ console.log(`
 Take a look at the 'requester' function.
 
 - What it does?
-- How it's used? Add different use-case examples that covers every functionality.
+
+  It's a request manager based in fetch library
+
+- How it's used?
+
+  Add different use-case examples that covers every functionality. See examples below.
+
 - How it is called this design pattern or technique?
+
+  It's based on factory design pattern.
 
 HINT: Use https://api.github.com/users/mediastream
 `);
@@ -16,7 +24,29 @@ HINT: Use https://api.github.com/users/mediastream
 // Add fetch polyfill for Node.js
 require('isomorphic-fetch'); // See: https://github.com/matthew-andrews/isomorphic-fetch
 
+/**
+ *
+ * @param method
+ * @param base
+ * @param headers
+ * @return {function(*=): Promise<any>}
+ */
 function requester(method, base, headers = { Accept: '*/*' }) {
   return (path = []) => fetch((base ? [base, ...path] : path).join('/'), { method, headers })
     .then(r => r.json());
 }
+
+const URL_BASE = 'https://api.github.com/users/mediastream';
+
+//Example with GET
+let fetcher = requester('GET', URL_BASE);
+fetcher().then(data => {
+  console.log("GET ", data);
+});
+
+//Example with POST
+fetcher = requester('POST', URL_BASE);
+fetcher().then(data => {
+  console.log("POST ", data);
+});
+
