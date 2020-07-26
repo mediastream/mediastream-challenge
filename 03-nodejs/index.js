596ffbc658888ef5918f5959e62d1c4cad167976
AryRosvall <ary.rosvall@gmail.com>
2020-07-25 20:45:22 -0500

    Challenge 03 completed

--- 03-nodejs/index.js
+++ 03-nodejs/index.js
@@ -18,15 +18,59 @@ $ node utils/seed.js
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const os = require('os');
 
 // Setup database
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/mediastream-challenge');
+mongoose.connect('mongodb://localhost/mediastream-challenge', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const User = require('./models/User');
+const { resolve } = require('path');
 
 // Setup Express.js app
 const app = express();
 
-// TODO
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+  try {
+    res.status(200)
+      .send(
+        "Mediastream-challenge"
+      );
+
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/users', downloadUsers);
+
+
+function csvTransform(obj) {
+  return `${Object.values(obj).join(", ")}${os.EOL}`
+}
+
+async function downloadUsers(req, res, next) {
+  try {
+    const csv = './tmp/test.csv';
+    const initTime = Date.now();
+
+    const stream = User.find().stream({ transform: csvTransform })
+    const output = await fs.createWriteStream("./tmp/test.csv")
+
+    await stream.pipe(output).on("finish", () => {
+      const ElapsedTime = Date.now() - initTime;
+      console.log(`Execution ended. Elapsed time: ${(ElapsedTime / 1000)} seconds`);
+      res.download(csv)
+    });
+
+  } catch (error) {
+    console.log("err", error)
+  }
+}
 
 app.listen(3000);
