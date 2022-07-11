2afeffd0cfcc42ca2267d183006cc3606077d2b3
rodrigo arancibia <rodrigoarancibiapla@gmail.com>
2022-07-11 19:10:19 -0400

    entrega de pruebas 1 y 2

---
+++ 03-react/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Exercise from './components/pages/Exercise'
+
+function App () {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Exercise} exact />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
