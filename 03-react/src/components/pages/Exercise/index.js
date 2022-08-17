c995497aac11eadcef9414ec91d46f6e89453fe5
WilliamsGaleano <williams.galeano@inetum.com>
2022-08-17 03:33:53 -0500

    finish Q3

--- 03-react/src/components/pages/Exercise/index.js
+++ 03-react/src/components/pages/Exercise/index.js
@@ -1,31 +1,36 @@
 import './assets/styles.css'
 import { React, useState } from 'react'
+import { isEqual } from 'lodash'
 
 export default function Exercise01 () {
-  const movies = [
+  const [movies, setMovies] = useState([
     {
       id: 1,
       name: 'Star Wars',
-      price: 20
+      price: 20,
+      quantity: 1
     },
     {
       id: 2,
       name: 'Minions',
-      price: 25
+      price: 25,
+      quantity: 1
     },
     {
       id: 3,
       name: 'Fast and Furious',
-      price: 10
+      price: 10,
+      quantity: 1
     },
     {
       id: 4,
       name: 'The Lord of the Rings',
-      price: 5
+      price: 5,
+      quantity: 1
     }
-  ]
+  ])
 
-  /*
+  const [carts, setCarts] = useState([])
   const discountRules = [
     {
       m: [3, 2],
@@ -39,19 +44,56 @@ export default function Exercise01 () {
       m: [4, 2],
       discount: 0.1
     }
-  ] */
+  ]
 
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20,
-      quantity: 2
+  const addToCart = (obj) => {
+    const gg = movies.filter(x => x.id !== obj.id)
+    setMovies(gg)
+    setCarts([...carts, obj])
+  }
+
+  const decrementQuantity = (objCart) => {
+    if (objCart.quantity - 1 === 0) {
+      const cartTemp = carts.filter(x => x.id !== objCart.id)
+      setCarts([...cartTemp])
+      setMovies([...movies, objCart].sort(orderDataById))
+    } else {
+      const cartsTemp = carts.map((x) => {
+        if (x.id === objCart.id) x.quantity -= 1
+        return x
+      })
+      setCarts([...cartsTemp])
     }
-  ])
+  }
 
-  setCart()
-  const getTotal = () => 0 // TODO: Implement this
+  const orderDataById = (a, b) => {
+    if (a.id === b.id) return 0
+    if (a.id < b.id) return -1
+    return 1
+  }
+
+  const incrementQuantity = (objCart) => {
+    const cartsTemp = carts.map((x) => {
+      if (x.id === objCart.id) x.quantity += 1
+      return x
+    })
+    setCarts([...cartsTemp])
+  }
+
+  const getTotal = () => {
+    if (carts.length !== 0) {
+      let sum = carts.reduce((accumulator, object) => {
+        return accumulator + object.quantity * object.price
+      }, 0)
+
+      const arrIds = carts.map(x => x.id)
+      const rule = discountRules.find(x => isEqual(x.m, arrIds))
+      sum = sum - sum * (rule === undefined ? 0 : rule.discount)
+      return `Total: $${sum} `.concat(rule === undefined ? '' : ` -> disc : ${rule?.discount * 100}%`)
+    } else {
+      return ''
+    }
+  }
 
   return (
     <section className="exercise01">
@@ -70,7 +112,7 @@ export default function Exercise01 () {
                   Price: ${o.price}
                 </li>
               </ul>
-              <button onClick={() => console.log('Add to cart', o)}>
+              <button onClick={() => addToCart(o)}>
                 Add to cart
               </button>
             </li>
@@ -79,7 +121,7 @@ export default function Exercise01 () {
       </div>
       <div className="movies__cart">
         <ul>
-          {cart.map((x, index) => (
+          {carts.map((x, index) => (
             <li key={index} className="movies__cart-card">
               <ul>
                 <li>
@@ -93,13 +135,13 @@ export default function Exercise01 () {
                 </li>
               </ul>
               <div className="movies__cart-card-quantity">
-                <button onClick={() => console.log('Decrement quantity', x)}>
+                <button onClick={() => decrementQuantity(x)}>
                   -
                 </button>
                 <span>
                   {x.quantity}
                 </span>
-                <button onClick={() => console.log('Increment quantity', x)}>
+                <button onClick={() => incrementQuantity(x)}>
                   +
                 </button>
               </div>
@@ -107,7 +149,7 @@ export default function Exercise01 () {
           ))}
         </ul>
         <div className="movies__cart-total">
-          <p>Total: ${getTotal()}</p>
+          <p>{getTotal()}</p>
         </div>
       </div>
     </section>
