edcfca0f03bc66617f5937601d4704bc7471c27f
Jaime Irazabal <jaimeirazabal1@sistemaimpulsa.com>
2022-09-11 13:24:54 -0400

    por el 3

--- 02-nodejs/models/User.js
+++ 02-nodejs/models/User.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose')
+// let LineInputStream = require("line-input-stream"),
+//     fs = require("fs"),
+//     async = require("async"),
+//     Schema = mongoose.Schema;
 
 // Setup database
 mongoose.Promise = Promise
-mongoose.connect('mongodb://localhost/mediastream-challenge')
+mongoose.connect('mongodb://localhost:27017/mediastream-challenge');
 
 const User = mongoose.model('User', {
   name: String,
   email: String
 })
+
+
 
 module.exports = User
