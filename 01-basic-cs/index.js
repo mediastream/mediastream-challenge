7dfa4b92fb665739e522ed982dc56bc38d357fee
cristopher Palacios <criszelaya24@hotmail.com>
2022-10-11 10:36:57 -0600

    Challengue 1

--- 01-basic-cs/index.js
+++ 01-basic-cs/index.js
@@ -1,11 +1,37 @@
 'use strict'
 
-const _ = require('lodash') // https://lodash.com/docs/4.17.4
 const assert = require('assert')
 
 const database = require('./database.json')
+const groupHats = (searchHatId) => {
+  return database.reduce((acc, seller) => {
+    seller.hats.forEach(hat => {
+      const hatId = searchHatId || hat.id
+      if (!acc[hatId]) acc[hatId] = 0
+      if (hat.id === hatId) acc[hatId] += 1
+    })
 
-const total = 0 // TODO
+    return acc
+  }, {})
+}
+const findTopHatsSold = ({ hats = {}, top = 3 }) => {
+  return Object.fromEntries(
+    Object.entries(hats)
+      .sort(([, a], [, b]) => b - a).slice(0, top))
+}
+
+const sumTotalHats = (hats = {}) => {
+  let total = 0
+  Object.values(hats).forEach(totalSold => {
+    total += totalSold
+  })
+
+  return total
+}
+
+const hats = groupHats()
+const topHats = findTopHatsSold({ hats, top: 3 })
+const total = sumTotalHats(topHats)
 
 // Throws error on failure
 assert.equal(total, 23, `Invalid result: ${total} != 23`)
