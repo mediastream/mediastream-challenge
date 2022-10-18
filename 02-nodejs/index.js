9ff1565965d40f8d47c51aa2dcb54cb434f2c58c
Maikoll Soto <maikoll120@gmail.com>
2022-10-18 15:26:46 -0400

    Challenge solved!

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -3,10 +3,33 @@
 const express = require('express')
 
 const User = require('./models/User')
+const Parser = require('json2csv').Parser
 
 // Setup Express.js app
 const app = express()
 
 // TODO: everything else
+app.get('/users', async (req, res) => {
+  try {
+    User.find().then((records) => {
+      const users = []
+
+      records.forEach((user) => {
+        const { id, name, email } = user
+        users.push({ id, name, email })
+      })
+
+      const headers = ['Id', 'Name', 'Email']
+      const parser = new Parser({ headers })
+      const data = parser.parse(users)
+
+      res.setHeader('Content-Type', 'text/csv')
+      res.setHeader('Content-Disposition', 'attachment; filename=users.csv')
+      res.status(200).end(data)
+    })
+  } catch (error) {
+    res.status(400).send(error?.message)
+  }
+})
 
 app.listen(3000)
---
+++ 03-react/src/components/MovieCart/MovieCart.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+const discountRules = [
+  {
+    m: [3, 2],
+    discount: 0.25
+  },
+  {
+    m: [2, 4, 1],
+    discount: 0.5
+  },
+  {
+    m: [4, 2],
+    discount: 0.1
+  }
+]
+
+const MovieCart = ({ cartList, handleQuantityCart }) => {
+  const getTotal = () => {
+    let total = cartList.reduce((total, movie) => { return total + (movie.price * movie.quantity) }, 0)
+
+    // Filter discount rules
+    const cartIdList = cartList.map(movie => movie.id)
+    const validRules = discountRules.filter(rule => {
+      return rule.m.length === cartIdList.length && rule.m.every((element) => cartIdList.includes(element))
+    })
+
+    // Apply discount if match
+    if (validRules.length === 1) {
+      total = total * (1 - validRules[0].discount)
+    }
+
+    return total
+  } // TODO: Implement this
+
+  return (
+    <div className="movies__cart">
+    <ul>
+      {cartList.map(x => (
+        <li key={x.id} className="movies__cart-card">
+          <ul>
+            <li>
+              ID: {x.id}
+            </li>
+            <li>
+              Name: {x.name}
+            </li>
+            <li>
+              Price: ${x.price}
+            </li>
+          </ul>
+          <div className="movies__cart-card-quantity">
+            <button onClick={() => handleQuantityCart(x, -1)}>
+              -
+            </button>
+            <span>
+              {x.quantity}
+            </span>
+            <button onClick={() => handleQuantityCart(x, 1)}>
+              +
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+    <div className="movies__cart-total">
+      <p>Total: ${getTotal()}</p>
+    </div>
+  </div>
+  )
+}
+
+export default MovieCart
