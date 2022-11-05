b3dc46b83cba49645b1827db523073543c217025
Randi Guarcas <rguarcas18@gmail.com>
2022-11-05 12:38:37 -0600

    Added challenge solved

--- 01-basic-cs/index.js
+++ 01-basic-cs/index.js
@@ -1,24 +1,24 @@
-"use strict";
+'use strict'
 
-const _ = require("lodash"); // https://lodash.com/docs/4.17.4
-const assert = require("assert");
+const _ = require('lodash') // https://lodash.com/docs/4.17.4
+const assert = require('assert')
 
-const database = require("./database.json");
+const database = require('./database.json')
 
-const hats = _.map(database, "hats").flat();
+const hats = _.map(database, 'hats').flat()
 
-const groupedHats = _(_.groupBy(hats, "id"))
+const groupedHats = _(_.groupBy(hats, 'id'))
   .map((group) => group.length)
-  .value();
+  .value()
 
-const sortedHats = _.sortBy(groupedHats).reverse();
+const sortedHats = _.sortBy(groupedHats).reverse()
 
-const total = _.sumBy(_.take(sortedHats, 3));
+const total = _.sumBy(_.take(sortedHats, 3))
 
 // Throws error on failure
-assert.equal(total, 23, `Invalid result: ${total} != 23`);
+assert.equal(total, 23, `Invalid result: ${total} != 23`)
 
-console.log("Success!");
+console.log('Success!')
 
 /**
  * Time and space complexity in O() notation is:
--- 02-nodejs/controllers/user.controller.js
+++ 02-nodejs/controllers/user.controller.js
@@ -1,30 +1,30 @@
-const User = require("../models/User");
+const User = require('../models/User')
 
 const userController = {
   download: async (req, res) => {
     try {
-      const fileName = Date.now();
-      res.setHeader("Content-Encoding", "UTF-8");
-      res.setHeader("Content-Type", "text/csv; charset=UTF-8");
+      const fileName = Date.now()
+      res.setHeader('Content-Encoding', 'UTF-8')
+      res.setHeader('Content-Type', 'text/csv; charset=UTF-8')
       res.setHeader(
-        "Content-Disposition",
+        'Content-Disposition',
         `attachment; filename=${fileName}.csv`
-      );
-      res.status(200);
+      )
+      res.status(200)
 
       await User.find()
         .lean()
         .cursor()
-        .on("data", (user) => {
-          res.write(`${user.name},${user.email}\n`);
+        .on('data', (user) => {
+          res.write(`${user.name},${user.email}\n`)
         })
-        .on("end", () => {
-          res.end();
-        });
+        .on('end', () => {
+          res.end()
+        })
     } catch (e) {
-      res.status(500).send("Internal Error").end();
+      res.status(500).send('Internal Error').end()
     }
-  },
-};
+  }
+}
 
-module.exports = userController;
+module.exports = userController
