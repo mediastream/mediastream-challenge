2e9639245e890852e963cdcaf06a890e0cc3edec
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-03-29 15:26:39 -0300

    Implement `/users` streaming download to CSV file

--- 02-nodejs/controllers/user.js
+++ 02-nodejs/controllers/user.js
@@ -1,6 +1,27 @@
-const getAll = async (_, res) => res.status(501).json({
-  message: 'Not implemented'
-})
+const user = require('../models/user')
+const stream = require('node:stream')
+
+const getAll = async (_, res) => {
+  try {
+    const result = await user.getAll()
+
+    const csvArray = result.map(item => `${item._id},${item.name},${item.email}`)
+    csvArray.unshift('id,name,email')
+    const csvData = Buffer.from(csvArray.join('\n'), 'utf8')
+
+    const readStream = new stream.PassThrough()
+    readStream.end(csvData)
+
+    res.set('Content-Type', 'text/csv')
+    res.set('Content-Disposition', 'attachment; filename="users.csv"')
+    readStream.pipe(res)
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({
+      message: 'Could not retrieve users'
+    })
+  }
+}
 
 module.exports = {
   getAll
