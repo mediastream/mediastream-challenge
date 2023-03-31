cdb1ab39104913bfdb3ace55c1b5aebcd40d81f6
Joel Jaime <joel.alexander.jaime@gmail.com>
2023-03-31 12:07:38 -0400

    :sparkles: Challenge 02 - Completado

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -7,6 +7,29 @@ const User = require('./models/User')
 // Setup Express.js app
 const app = express()
 
-// TODO: everything else
+const PORT = 3000
+const PATH = '/users'
 
-app.listen(3000)
+// Route for streaming users
+app.get(PATH, async (req, res, next) => {
+  try {
+    // Crenado un cursor del stream de usuarios
+    const cursor = User.find().cursor()
+
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Content-Disposition', 'attachment; filename="users.json"')
+
+    // Haciendo el stream de usuarios como objeto JSON
+    cursor.on('data', (usuario) => res.write(JSON.stringify(usuario)))
+
+    // Terminando el stream cuando no existan mas usuarios
+    cursor.on('end', () => res.end())
+  } catch (err) {
+    next(err)
+  }
+})
+
+// TODO: everything else
+app.listen(PORT, () => {
+  console.log(`> Service created in http://localhost:${PORT}${PATH}`)
+})
