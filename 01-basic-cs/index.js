b0f140dbcdbed8c7dddb248116147993e5ddcb1a
Sergio Garcia <sagg_301@outlook.com>
2023-03-31 15:57:08 -0400

    chore: Challenge solution by Sergio Garcia

--- 01-basic-cs/index.js
+++ 01-basic-cs/index.js
@@ -4,8 +4,14 @@ const _ = require('lodash') // https://lodash.com/docs/4.17.4
 const assert = require('assert')
 
 const database = require('./database.json')
-
-const total = 0 // TODO
+const total = _.chain(database)
+  .flatMap('hats') // Get all the hats
+  .groupBy('id') // Group all records by Id
+  .orderBy('length', 'desc') // Order by lenght from top to bottom
+  .slice(0, 3) // Get top three
+  .flatMap() // Flatten all to get all records
+  .size() // Count
+  .value()
 
 // Throws error on failure
 assert.equal(total, 23, `Invalid result: ${total} != 23`)
@@ -14,6 +20,6 @@ console.log('Success!')
 
 /**
  * Time and space complexity in O() notation is:
- *   - time complexity: TODO
- *   - space complexity: TODO
+ *   - time complexity: O(n*log(n))
+ *   - space complexity: O(n)
  */
