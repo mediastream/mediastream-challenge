5194167b41abeafa66bbb85079742080b995d9c4
Jaime Fonseca <fonsecajaime6a@gmail.com>
2023-03-31 15:11:57 -0600

    fix lints

--- 01-basic-cs/index.js
+++ 01-basic-cs/index.js
@@ -6,24 +6,24 @@ const assert = require('assert')
 const database = require('./database.json')
 
 const getTopThreeSellingHats = () => {
-    // Getting all hats from database
-    const allHats = _.flatMap(database,"hats")
+  // Getting all hats from database
+  const allHats = _.flatMap(database, 'hats')
 
-    //Count all hats grouping by Id
-    const countHatsById = _.countBy(allHats,"id")
+  // Count all hats grouping by Id
+  const countHatsById = _.countBy(allHats, 'id')
 
-    //Make an object from array
-    const collectionCountHats = _.map(countHatsById, (amount, id) => ({id, amount}))
+  // Make an object from array
+  const collectionCountHats = _.map(countHatsById, (amount, id) => ({ id, amount }))
 
-    //order objects with lodash by amount descending
-    const orderedHatsByAmount = _.orderBy(collectionCountHats, "amount", "desc")
-    
-    // Get top three selling hats
-    return _.take(orderedHatsByAmount,3);
+  // order objects with lodash by amount descending
+  const orderedHatsByAmount = _.orderBy(collectionCountHats, 'amount', 'desc')
+
+  // Get top three selling hats
+  return _.take(orderedHatsByAmount, 3)
 }
 
-//Sum all three hats amounts for the total
-const total = _.sumBy(getTopThreeSellingHats(),"amount")
+// Sum all three hats amounts for the total
+const total = _.sumBy(getTopThreeSellingHats(), 'amount')
 
 // Throws error on failure
 assert.equal(total, 23, `Invalid result: ${total} != 23`)
--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -8,16 +8,14 @@ const User = require('./models/User')
 // Setup Express.js app
 const app = express()
 
-app.get("/users",async (req,res) => {
-    res.setHeader('Content-Type', 'text/csv')
-    res.setHeader('Content-Disposition', 'attachment; filename=users.csv')
+app.get('/users', async (req, res) => {
+  res.setHeader('Content-Type', 'text/csv')
+  res.setHeader('Content-Disposition', 'attachment; filename=users.csv')
 
-    let allUsers = await User.find({})
-    console.log(allUsers)
-    fastCsv
+  const allUsers = await User.find({})
+  fastCsv
     .write(allUsers.map(u => u.toObject()), { headers: true })
-    .pipe(res);
-    
-});
+    .pipe(res)
+})
 
 app.listen(3000)
