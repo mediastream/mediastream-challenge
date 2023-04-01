32ee66ae2cf1f09c0abffbaa3bbe04e1b54a7d94
Luis Mera <luismera92@gmail.com>
2023-04-01 14:49:34 -0500

    feat: solution all challenge Luis Alberto Mera

--- 01-basic-cs/index.js
+++ 01-basic-cs/index.js
@@ -5,7 +5,26 @@ const assert = require('assert')
 
 const database = require('./database.json')
 
-const total = 0 // TODO
+function findTopSellingHatsSum (hatsData) {
+  // Group hats by model and count how many of each model were sold
+  const groupedHats = _.groupBy(hatsData, 'id')
+  const hatCounts = _.map(groupedHats, (hats, model) => {
+    return {
+      model,
+      count: hats.length
+    }
+  })
+
+  // Sort hatCounts in descending order and sum the counts of the top 3 hats
+  const sortedHatCounts = _.orderBy(hatCounts, ['count'], ['desc'])
+  const topThreeHatCounts = _.take(sortedHatCounts, 3)
+  const sum = _.sumBy(topThreeHatCounts, 'count')
+  return sum
+}
+
+const hats = _.flattenDeep(database.map(obj => obj.hats ? obj.hats : obj))
+
+const total = findTopSellingHatsSum(hats) // TODO
 
 // Throws error on failure
 assert.equal(total, 23, `Invalid result: ${total} != 23`)
