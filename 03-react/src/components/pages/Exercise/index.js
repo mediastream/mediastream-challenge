c075648eb29847ac854ca567cefa76e4db0b49dd
Lazaro Estrada <lazaromer97@gmail.com>
2023-04-05 08:01:18 -0400

    add: 03-react solution

--- 03-react/src/components/pages/Exercise/index.js
+++ 03-react/src/components/pages/Exercise/index.js
@@ -1,5 +1,5 @@
+import React, { useState } from 'react'
 import './assets/styles.css'
-import { useState } from 'react'
 
 export default function Exercise01 () {
   const movies = [
@@ -40,23 +40,88 @@ export default function Exercise01 () {
     }
   ]
 
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20,
-      quantity: 2
-    }
-  ])
+  const [cart, setCart] = useState([])
 
-  const getTotal = () => 0 // TODO: Implement this
+  /**
+   * The function calculates the total price of items in a shopping cart based on their individual prices
+   * and quantities.
+   */
+  const getTotal = () => cart.reduce((total, movie) => total + movie.price * movie.quantity, 0)
+
+  /**
+   * This function calculates the total price of items in a cart with applied discounts based on a set of
+   * rules.
+   * @returns The function `getTotalWithDiscounts` is returning the total price of the items in the cart
+   * after applying any applicable discounts based on the `discountRules` array.
+   */
+  const getTotalWithDiscounts = () => {
+    const total = getTotal()
+    let discount = 0
+    const moviesIdsInCart = cart.map(movie => movie.id)
+    for (let i = 0; i < discountRules.length; i++) {
+      if (discountRules[i].m.every(m => moviesIdsInCart.includes(m))) {
+        discount += discountRules[i].discount
+      }
+    }
+
+    return total - discount
+  }
+
+  /**
+   * This function adds a movie to the cart if it is not already in the cart.
+   */
+  const addToCar = movie => {
+    const isMovieInCart = cart.some((cartMovie) => cartMovie.id === movie.id)
+    if (!isMovieInCart) {
+      setCart([...cart, { ...movie, quantity: 1 }])
+    }
+  }
+
+  /**
+   * The function increases the quantity of a movie in the cart by one.
+   * @param id - The `id` parameter is a unique identifier for a movie in the `cart` array. It is used to
+   * find the movie object in the array and update its quantity property.
+   */
+  const increaseQuantity = (id) => {
+    setCart(cart.map(movie => {
+      if (movie.id === id) {
+        return {
+          ...movie,
+          quantity: (movie.quantity || 0) + 1
+        }
+      }
+      return movie
+    }))
+  }
+
+  /**
+   * This function decreases the quantity of a movie in the cart and removes it if the quantity becomes
+   * zero.
+   * @param id - The `id` parameter is a unique identifier for a movie in the `cart` array. It is used to
+   * find the movie in the array and decrease its quantity by 1.
+   */
+  const decreaseQuantity = (id) => {
+    setCart(cart.map(movie => {
+      if (movie.id === id) {
+        const newQuantity = (movie.quantity || 0) - 1
+        if (newQuantity === 0) {
+          return null
+        }
+        return {
+          ...movie,
+          quantity: newQuantity
+        }
+      }
+      return movie
+    }).filter(Boolean))
+  }
 
   return (
     <section className="exercise01">
       <div className="movies__list">
         <ul>
           {movies.map(o => (
-            <li className="movies__list-card">
+            <li className="movies__list-card" key={o.id}>
               <ul>
                 <li>
                   ID: {o.id}
@@ -68,7 +133,7 @@ export default function Exercise01 () {
                   Price: ${o.price}
                 </li>
               </ul>
-              <button onClick={() => console.log('Add to cart', o)}>
+              <button onClick={ () => addToCar(o)}>
                 Add to cart
               </button>
             </li>
@@ -78,7 +143,7 @@ export default function Exercise01 () {
       <div className="movies__cart">
         <ul>
           {cart.map(x => (
-            <li className="movies__cart-card">
+            <li className="movies__cart-card" key={x.id}>
               <ul>
                 <li>
                   ID: {x.id}
@@ -91,13 +156,13 @@ export default function Exercise01 () {
                 </li>
               </ul>
               <div className="movies__cart-card-quantity">
-                <button onClick={() => console.log('Decrement quantity', x)}>
+                <button onClick={() => decreaseQuantity(x.id)}>
                   -
                 </button>
                 <span>
                   {x.quantity}
                 </span>
-                <button onClick={() => console.log('Increment quantity', x)}>
+                <button onClick={() => increaseQuantity(x.id)}>
                   +
                 </button>
               </div>
@@ -105,7 +170,7 @@ export default function Exercise01 () {
           ))}
         </ul>
         <div className="movies__cart-total">
-          <p>Total: ${getTotal()}</p>
+          <p>Total: ${getTotalWithDiscounts()}</p>
         </div>
       </div>
     </section>
