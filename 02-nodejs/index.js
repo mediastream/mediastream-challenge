ef3f523f7fc8882c84b3474918ff90d6f03db973
Lazaro Estrada <lazaromer97@gmail.com>
2023-04-05 06:14:14 -0400

    add: 02-nodejs solution

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -7,6 +7,21 @@ const User = require('./models/User')
 // Setup Express.js app
 const app = express()
 
-// TODO: everything else
+/**
+ * This code sets up a GET route for the '/users' endpoint. When a GET request is made to this
+ * endpoint, it retrieves all users from the database using the `User.find({})` method and stores them
+ * in the `users` variable. It then creates a CSV file with the headers "Name" and "Email" and adds
+ * each user's name and email to the file. Finally, it sets the response headers to indicate that the
+ * response is a CSV file and attaches the file to the response before sending it back to the client.
+ **/
+app.get('/users', async (req, res) => {
+  console.log('HERE')
+  const users = await User.find({}).exec()
+  let content = 'Name, Email\r\n'
+  users.forEach(user => { content += `${user.name},${user.email}\r\n` })
+  res.header('Content-Type', 'text/csv')
+  res.attachment('users.csv')
+  res.send(content)
+})
 
 app.listen(3000)
