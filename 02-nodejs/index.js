81f766884ff8ac14bbd53662e8c212ce5989f611
jvelazquez99 <jose.velazquez@wepa.com.py>
2023-04-06 14:29:04 -0400

    feat: challenge 2

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -1,12 +1,43 @@
 'use strict'
 
 const express = require('express')
+const fs = require('fs')
 
 const User = require('./models/User')
 
 // Setup Express.js app
 const app = express()
 
-// TODO: everything else
+app.get('/users', async (req, res) => {
+  try {
+    await saveAllUsers()
+    res.json({ message: 'OK' })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Internal Server Error' })
+  }
+})
+
+async function saveAllUsers () {
+  return new Promise((resolve, reject) => {
+    const csvStream = fs.createWriteStream('users.csv')
+    csvStream.write('id,name,email\n')
+
+    // As we have limited memory resources we load the users from the database in batches of 100 documents
+    const usersCursor = User.find().batchSize(100).cursor()
+    usersCursor
+      .on('data', function (doc) {
+        const { _id, name, email } = doc._doc
+        csvStream.write(`${_id},${name},${email}\n`)
+      })
+      .on('error', function (err) {
+        reject(err)
+      })
+      .on('end', function () {
+        console.log('Data has been written to users.csv')
+        resolve()
+      })
+  })
+}
 
 app.listen(3000)
