4e3ebf2a7d66071c5888dd7b34f907ce1cda85d2
andresportillov <39763652+andresportillov@users.noreply.github.com>
2023-07-19 00:18:35 -0400

    Challenge Completed

--- 01-basic-cs/index.js
+++ 01-basic-cs/index.js
@@ -5,9 +5,11 @@ const assert = require('assert')
 
 const database = require('./database.json')
 
-const total = 0 // TODO
+const mostSoldHats2 = _.flatMap(database, (n) => n.hats)
+const count = _.countBy(mostSoldHats2, 'id')
+const ordered = _.orderBy(count, null, 'desc').slice(0, 3)
+const total = _.sum(ordered)
 
-// Throws error on failure
 assert.equal(total, 23, `Invalid result: ${total} != 23`)
 
 console.log('Success!')
@@ -17,3 +19,11 @@ console.log('Success!')
  *   - time complexity: TODO
  *   - space complexity: TODO
  */
+/*
+Space complexity: O(n), where n is the number of hats in the database.
+This is because we create a new array mostSoldHats that contains all the hats in the database, and then we create a new object count that contains the count of each hat. Finally, we create a new array ordered that contains the top 3 most sold hats. All of these data structures require space proportional to the number of hats in the database.
+
+Time complexity: O(n log n), where n is the number of hats in the database.
+This is because we use the flatMap method to create the mostSoldHats array, which takes O(n) time. Then we use the countBy method to create the count object, which also takes O(n) time. Finally, we use the orderBy method to sort the count object, which takes O(n log n) time. The slice method takes constant time, so it doesn't affect the overall time complexity. Therefore, the overall time complexity is O(n log n).
+
+*/
--- 03-react/src/components/pages/Exercise/index.js
+++ 03-react/src/components/pages/Exercise/index.js
@@ -1,74 +1,112 @@
+import _ from 'lodash'
 import './assets/styles.css'
-import { useState } from 'react'
+import React, { useMemo, useState } from 'react'
+
+const discountRules = [
+  {
+    m: [3, 2], // [1, 2 ,3]
+    discount: 0.25
+  }, // 140 * (1 - 0.25)
+  {
+    m: [2, 4, 1],
+    discount: 0.5
+  },
+  {
+    m: [4, 2],
+    discount: 0.1
+  }
+]
+
+const movies = [
+  {
+    id: 1,
+    name: 'Star Wars',
+    price: 20
+  },
+  {
+    id: 2,
+    name: 'Minions',
+    price: 25
+  },
+  {
+    id: 3,
+    name: 'Fast and Furious',
+    price: 10
+  },
+  {
+    id: 4,
+    name: 'The Lord of the Rings',
+    price: 5
+  }
+]
+
+const getTotal = (cart) => {
+  const total = _.reduce(cart, (acc, movie) => acc + (movie.price * movie.quantity), 0)
+  const cartIds = cart.map(movie => movie.id)
+  return discountRules.reduce((acc, rule) => {
+    if (rule.m.length === cartIds.length) {
+      if (_.intersection(rule.m, cartIds).length === rule.m.length) {
+        return acc * (1 - rule.discount)
+      }
+    }
+    return acc
+  }, total)
+}
 
 export default function Exercise01 () {
-  const movies = [
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20
-    },
-    {
-      id: 2,
-      name: 'Minions',
-      price: 25
-    },
-    {
-      id: 3,
-      name: 'Fast and Furious',
-      price: 10
-    },
-    {
-      id: 4,
-      name: 'The Lord of the Rings',
-      price: 5
-    }
-  ]
+  const [cart, setCart] = useState([])
+  const total = useMemo(() => getTotal(cart), [cart])
 
-  const discountRules = [
-    {
-      m: [3, 2],
-      discount: 0.25
-    },
-    {
-      m: [2, 4, 1],
-      discount: 0.5
-    },
-    {
-      m: [4, 2],
-      discount: 0.1
+  const handleAddToCart = (product) => {
+    const productInCart = cart.find(
+      (cartProduct) => cartProduct.id === product.id
+    )
+    if (productInCart) {
+      handleAddMovie(product.id)
+    } else {
+      setCart((_cart) => [..._cart, { ...product, quantity: 1 }])
     }
-  ]
+  }
 
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20,
-      quantity: 2
+  const handleAddMovie = (productId) => {
+    setCart((_cart) =>
+      _cart.map((cartProduct) =>
+        cartProduct.id === productId
+          ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+          : cartProduct
+      )
+    )
+  }
+
+  const handleRemoveMovie = (productId, quantity) => {
+    const result = quantity - 1
+    if (result === 0) {
+      setCart((_cart) =>
+        _cart.filter((cartProduct) => cartProduct.id !== productId)
+      )
+    } else {
+      setCart((_cart) =>
+        _cart.map((cartProduct) =>
+          cartProduct.id === productId
+            ? { ...cartProduct, quantity: result }
+            : cartProduct
+        )
+      )
     }
-  ])
-
-  const getTotal = () => 0 // TODO: Implement this
+  }
 
   return (
     <section className="exercise01">
       <div className="movies__list">
         <ul>
-          {movies.map(o => (
-            <li className="movies__list-card">
+          {movies.map((movie) => (
+            <li key={movie.id} className="movies__list-card">
               <ul>
-                <li>
-                  ID: {o.id}
-                </li>
-                <li>
-                  Name: {o.name}
-                </li>
-                <li>
-                  Price: ${o.price}
-                </li>
+                <li>ID: {movie.id}</li>
+                <li>Name: {movie.name}</li>
+                <li>Price: ${movie.price}</li>
               </ul>
-              <button onClick={() => console.log('Add to cart', o)}>
+              <button onClick={() => handleAddToCart(movie)}>
                 Add to cart
               </button>
             </li>
@@ -77,35 +115,27 @@ export default function Exercise01 () {
       </div>
       <div className="movies__cart">
         <ul>
-          {cart.map(x => (
-            <li className="movies__cart-card">
+          {cart.map((cartMovie) => (
+            <li key={cartMovie.id} className="movies__cart-card">
               <ul>
-                <li>
-                  ID: {x.id}
-                </li>
-                <li>
-                  Name: {x.name}
-                </li>
-                <li>
-                  Price: ${x.price}
-                </li>
+                <li>ID: {cartMovie.id}</li>
+                <li>Name: {cartMovie.name}</li>
+                <li>Price: ${cartMovie.price}</li>
               </ul>
               <div className="movies__cart-card-quantity">
-                <button onClick={() => console.log('Decrement quantity', x)}>
+                <button
+                  onClick={() => handleRemoveMovie(cartMovie.id, cartMovie.quantity)}
+                >
                   -
                 </button>
-                <span>
-                  {x.quantity}
-                </span>
-                <button onClick={() => console.log('Increment quantity', x)}>
-                  +
-                </button>
+                <span>{cartMovie.quantity}</span>
+                <button onClick={() => handleAddMovie(cartMovie.id)}>+</button>
               </div>
             </li>
           ))}
         </ul>
         <div className="movies__cart-total">
-          <p>Total: ${getTotal()}</p>
+          <p>{cart.length > 0 ? `Total: $${total}` : 'No movies added to the cart'}</p>
         </div>
       </div>
     </section>
